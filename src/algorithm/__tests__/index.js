f1d0defb6c9f3ec0340518ea5c8e989ee3076741
Robert Smith <rbrtsmith84@googlemail.com>
2017-10-15 12:59:35 +0100

    Fix broken nextTick test

--- src/algorithm/__tests__/index.js
+++ src/algorithm/__tests__/index.js
@@ -1,44 +1,16 @@
 import nextTick from '../'
-import { twoOrThreeNeighbours, threeNeighbours } from '../quantityOfNeighbours'
-
-const grid = [
-  [{ val: -1 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }],
-  [{ val: 0 }, { val: 1 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }],
-  [{ val: 0 }, { val: 0 }, { val: 1 }, { val: 1 }, { val: 1 }, { val: 0 }],
-  [{ val: 0 }, { val: 0 }, { val: 0 }, { val: 1 }, { val: 0 }, { val: 0 }],
-  [{ val: 0 }, { val: 1 }, { val: 1 }, { val: 1 }, { val: 0 }, { val: 0 }],
-  [{ val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }]
-]
-
-describe('populated cell', () => {
-  it('it survives if it has two or three neighbours', () => {
-    expect(twoOrThreeNeighbours([1, 1], grid)).toBe(false)
-    expect(twoOrThreeNeighbours([2, 2], grid)).toBe(true)
-    expect(twoOrThreeNeighbours([2, 3], grid)).toBe(true)
-    expect(twoOrThreeNeighbours([2, 4], grid)).toBe(true)
-    expect(twoOrThreeNeighbours([3, 3], grid)).toBe(false)
-    expect(twoOrThreeNeighbours([4, 1], grid)).toBe(false)
-    expect(twoOrThreeNeighbours([4, 2], grid)).toBe(true)
-    expect(twoOrThreeNeighbours([4, 3], grid)).toBe(true)
-  })
-})
-
-describe('empty cell', () => {
-  it('becomes populated if it has three neighbours', () => {
-    expect(threeNeighbours([0, 0], grid)).toBe(false)
-    expect(threeNeighbours([1, 0], grid)).toBe(false)
-    expect(threeNeighbours([1, 2], grid)).toBe(true)
-    expect(threeNeighbours([2, 1], grid)).toBe(false)
-    expect(threeNeighbours([3, 0], grid)).toBe(false)
-    expect(threeNeighbours([3, 1], grid)).toBe(true)
-    expect(threeNeighbours([5, 1], grid)).toBe(false)
-    expect(threeNeighbours([5, 2], grid)).toBe(true)
-    expect(threeNeighbours([5, 3], grid)).toBe(false)
-  })
-})
 
 describe('next tick', () => {
   it('returns a new updated grid having executed the rules', () => {
+    const input = [
+      [{ val: -1 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }],
+      [{ val: 0 }, { val: 1 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }],
+      [{ val: 0 }, { val: 0 }, { val: 1 }, { val: 1 }, { val: 1 }, { val: 0 }],
+      [{ val: 0 }, { val: 0 }, { val: 0 }, { val: 1 }, { val: 0 }, { val: 0 }],
+      [{ val: 0 }, { val: 1 }, { val: 1 }, { val: 1 }, { val: 0 }, { val: 0 }],
+      [{ val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }]
+    ]
+
     const expected = [
       [{ val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }, { val: 0 }],
       [{ val: 0 }, { val: -1 }, { val: 1 }, { val: 1 }, { val: 0 }, { val: 0 }],
@@ -47,7 +19,7 @@ describe('next tick', () => {
       [{ val: 0 }, { val: -1 }, { val: 1 }, { val: 1 }, { val: 0 }, { val: 0 }],
       [{ val: 0 }, { val: 0 }, { val: 1 }, { val: 0 }, { val: 0 }, { val: 0 }]
     ]
-    expect(nextTick(grid)).toEqual(expected)
+    expect(nextTick(input)).toEqual(expected)
   })
 
   it('removes dead cells', () => {
